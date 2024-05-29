10dc795ee9c72bebd431aa130307ea2f05b6b0b0
Avinash174 <avinashmagar15@gmail.com>
2024-05-29 15:35:03 +0530

    new changes done

---
+++ lib/service/weather_service.dart
@@ -0,0 +1,21 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:weather/model/weather_model.dart';
+
+class WeatherService {
+  static const BASE_URL = 'http://api.openweathermap.org/data/2.5/weather';
+  final String apikey;
+
+  WeatherService(this.apikey);
+
+  Future<Weather> getWeather(String cityName) async {
+    final response = await http
+        .get(Uri.parse('$BASE_URL?q=$cityName&appid=$apikey&units=metric'));
+    if (response.statusCode == 200) {
+      return Weather.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to load weather data');
+    }
+  }
+}
