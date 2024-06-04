0ecf363234224cff3a2411b292472d7cdca9224f
Avinash174 <avinashmagar15@gmail.com>
2024-06-04 14:38:43 +0530

    new changes done

--- lib/service/weather_service.dart
+++ lib/service/weather_service.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:geolocator/geolocator.dart';
 import 'package:http/http.dart' as http;
 import 'package:weather/model/weather_model.dart';
 
@@ -17,5 +18,16 @@ class WeatherService {
     } else {
       throw Exception('Failed to load weather data');
     }
+  }
+
+  Future<String> getCurrentCity() async {
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+    }
+
+    Position? position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    List<Placemark>placemarks=await placemarkCordinate
   }
 }
